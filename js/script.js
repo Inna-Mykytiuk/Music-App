311eacb0eb16a64c9786bff35a2395e2b833a676
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-02-10 16:21:56 +0200

    add Day and Night mode

--- js/script.js
+++ js/script.js
@@ -148,3 +148,18 @@ toggles.forEach((toggle) => {
     titles.classList.toggle("active-bg");
   });
 });
+
+// Day and night
+
+function switchMode() {
+  let moon = document.getElementById("moon");
+  if (moon.className == "moon") {
+    moon.className = "sun";
+    document.body.style.backgroundColor = "#141D26";
+    document.body.style.color = "#fff";
+  } else {
+    moon.className = "moon";
+    document.body.style.backgroundColor = "#fff";
+    document.body.style.color = "#000";
+  }
+}
